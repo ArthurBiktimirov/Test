df0c9779f9f49d0e8dd363360f74559f8bcff2c0
Artur <biktimirov.am@phystech.edu>
2024-03-27 23:18:44 +0300

    update test_s

--- test_s.cpp
+++ test_s.cpp
@@ -46,15 +46,32 @@ void test6() {
 
 void test7() {
   Stack stack = *(create_empty_stack());
-  push (stack, 1);
-  push (stack, 2);
-  push (stack, 3);
+  push(stack, 1);
+  push(stack, 2);
+  push(stack, 3);
   if ((stack.HEAD->key == 1) and (stack.HEAD->next->key == 2) and (stack.HEAD->next->next->key == 3) and (size(stack) == 3) and (stack.TAIL->key == 3)) {
     std::cout << "Test 7: OK" << std::endl;
   }
 }
 
+void test8() {
+  Stack stack = *(create_empty_stack());
+  push(stack, 1);
+  push(stack, 2);
+  push(stack, 3);
+  bool correct = 1;
+  for (int i = 0; i < 3; ++i) {
+    if ((top(stack))->key != 3 - i){
+      correct = 0;
+    }
+    pop(stack);
+  }
+  if (correct and (top(stack) == stack.NIL)) {
+    std::cout << "Test 8: OK" << std::endl; 
+  }
+}
+
 int main() {
-  test1(); test2(); test3(); test4(); test5(); test6(); test7();
+  test1(); test2(); test3(); test4(); test5(); test6(); test7(); test8();
   return 0;
 }
